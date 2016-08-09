5d99ffdddb706a5674cfe388319c652601738099
Hiroshi Manabe <manabe.hiroshi@gmail.com>
2016-08-09 17:03:47 +0900

    Added --newline option to sentence_joiner.pl.

--- scripts/sentence_joiner.pl
+++ scripts/sentence_joiner.pl
@@ -4,14 +4,22 @@ use strict;
 use utf8;
 use open IO => ':utf8';
 use open ':std';
+use Getopt::Long;
 
 $| = 1;
+
+my $opt_newline = 0;
+
+GetOptions('newline' => \$opt_newline);
+
+my $delim = $opt_newline ? "\n" : " ";
 
 my $sentence = '';
 my $is_first = 1;
 while (<STDIN>) {
     chomp;
     if ($_ eq '') {
+        $sentence .= $delim if $opt_newline;
         print $sentence."\n";
         $sentence = '';
         $is_first = 1;
@@ -19,7 +27,7 @@ while (<STDIN>) {
     else {
         my @F = split /\t/, $_;
         $F[0] =~ s/^ //;
-        $sentence .= ' ' if $F[1] eq "1" and not $is_first;
+        $sentence .= $delim if $F[1] eq "1" and not $is_first;
         $sentence .= $F[0];
         $is_first = 0;
     }
