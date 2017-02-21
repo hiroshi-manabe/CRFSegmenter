e8c6624ec7aa6ca90a3961d63c3e2fc55fa6e27c
Hiroshi Manabe <manabe.hiroshi@gmail.com>
2017-02-21 10:40:57 +0900

    Implemented encryption.

--- Utility/EncryptionUtil.cpp
+++ Utility/EncryptionUtil.cpp
@@ -1,13 +1,38 @@
-#include "EncryptionUtil.h"
+#include "EncodeUtil.h"
 
 #include <cstddef>
+#include <cstring>
 
 namespace Utility {
 
-void encrypt(uint32_t code, char *buf, size_t size) {
+static uint32_t xorshift32(uint32_t y) {
+    y ^= y << 13;
+    y ^= y >> 17;
+    y ^= y << 15;
+    return y;
 }
 
-void decrypt(char *buf, size_t size) {
+void encodeDecode(uint32_t code, char *buf, size_t size) {
+    uint32_t *p = (uint32_t *)buf;
+    for (size_t i = 0; i < size / 4; ++i) {
+        p[i] ^= code;
+        code = xorshift32(code);
+    }
+    if (size % 4) {
+        uint32_t t = 0;
+        memcpy(&t, &buf[(size / 4) * 4], size % 4);
+        t ^= code;
+        memcpy(&buf[(size / 4) * 4], &t, size % 4);
+    }
+}
+
+void encode(uint32_t code, char *buf, size_t size) {
+    encodeDecode(code, buf, size);
+}
+
+void decode(char *buf, size_t size) {
+    uint32_t code = *(uint32_t *)buf;
+    encodeDecode(code, buf, size);
 }
 
 }  // namespace Utility
