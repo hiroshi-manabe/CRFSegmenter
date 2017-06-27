e975c25070466831c3dc6be4f506c0be4205833c
Hiroshi Manabe <manabe.hiroshi@gmail.com>
2017-06-27 10:29:56 +0900

    Fixed encryption.

--- Utility/EncryptionUtil.cpp
+++ Utility/EncryptionUtil.cpp
@@ -4,7 +4,7 @@
 
 namespace Utility {
 
-void encrypt(uint32_t code, char *buf, size_t size) {
+void encrypt(char *buf, size_t size) {
 }
 
 void decrypt(char *buf, size_t size) {
--- Utility/EncryptionUtil.h
+++ Utility/EncryptionUtil.h
@@ -6,7 +6,7 @@
 
 namespace Utility {
 
-void encrypt(uint32_t code, char *buf, size_t size);
+void encrypt(char *buf, size_t size);
 void decrypt(char *buf, size_t size);
 
 }  // namespace Utility
