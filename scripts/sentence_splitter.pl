90dd4fbfbece131e5f9cd5c84ff5effae8aa5fc1
Hiroshi Manabe <manabe.hiroshi@gmail.com>
2017-11-14 17:07:02 +0900

    Updated sentence_splitter.pl to treat abugida scripts.

--- scripts/sentence_splitter.pl
+++ scripts/sentence_splitter.pl
@@ -1,12 +1,17 @@
 #!/usr/bin/env perl
-
 use strict;
+
 use utf8;
+use Encode;
 use open IO => ':utf8';
 use open ':std';
+use FindBin;
+use lib $FindBin::Bin;
+use AbgdData;
 
 use Getopt::Long qw(:config posix_default no_ignore_case gnu_compat);
-use IO::Handle;
+
+$| = 1;
 
 my $opt_concat = 0;
 my $opt_ignore_latin = 0;
@@ -38,8 +43,6 @@ $preprocess = ($opt_ignore_url || $opt_ignore_numbers || $opt_ignore_latin);
 
 while (<STDIN>) {
     chomp;
-    my $is_first = 1;
-    my $prev = "";
 
     my $preprocessed = to_hankaku($_);
     if ($preprocess) {
@@ -49,19 +52,55 @@ while (<STDIN>) {
         $preprocessed =~ s{([A-Za-z]+)}{ rotate_nonchar(); $nonchar x length($1); }ge if $opt_ignore_latin;
     }
 
-    my $prev_preprocessed_char = '';
-    while (s/^([\x20\xa0]?)(.)//) {
-        my $sp = $1;
-        my $ch = $2;
-        if ($ch eq "\t") {
-            $sp = ' ';
-            $ch = ' ';
+    my @chars = split //;
+    my @chunks = ();
+    
+    my $chunk;
+    for my $char(@chars) {
+        my $last = substr($chunk, -1, 1);
+        my $last_type = AbgdData::get_type($last);
+        my $char_type = AbgdData::get_type($char);
+        if ($last ne '' and not
+            (AbgdData::get_script($char) and
+             AbgdData::get_script($last) == AbgdData::get_script($char) and
+             (($last_type == AbgdData::CORE and $char_type == AbgdData::POST) or
+              ($last_type == AbgdData::POST and $char_type == AbgdData::POST) or
+              ($last_type == AbgdData::PREV) or
+              ($last_type == AbgdData::STACK or $char_type == AbgdData::STACK)))) {
+            push @chunks, $chunk;
+            $chunk = "";
         }
-        $preprocessed =~ s/^[\x20\xa0]?(.)//;
-        my $preprocessed_char = $1;
-        
+        $chunk .= $char;
+    }
+    push @chunks, $chunk if $chunk ne "";
+    for (my $i = 0; $i < scalar(@chunks); ++$i) {
+        my $pre_chunk = $chunks[$i];
+        my $post_chunk = $chunks[$i + 1];
+        if ($post_chunk =~ m{(?<![\x{102b}\x{102c}])\x{103a}(?!\x{1039})} and
+            AbgdData::get_type($pre_chunk) == AbgdData::get_type($post_chunk)) {
+            splice(@chunks, $i, 2, $chunks[$i].$chunks[$i + 1]);
+            --$i;
+        }
+    }
+
+    my $position = 0;
+    my $is_first = 1;
+    my $prev_preprocessed_char = '';
+    for (my $i = 0; $i < scalar(@chunks); ++$i) {
+        my $chunk = $chunks[$i];
+        my $sp = "";
+        if ($chunk =~ m{^[\x20\xa0]$} and $i < scalar(@chunks) - 1) {
+            $sp = $chunk;
+            ++$i;
+            $chunk = $chunks[$i];
+        }
+        if ($chunk eq "\t") {
+            $sp = " ";
+            $chunk = " ";
+        }
+        my $preprocessed_char = substr($preprocessed, $position + length($sp), 1);
+        $position += length($sp) + length($chunk);
         my $print_sp = ($is_first or (not $opt_concat and $sp eq "\x20") or ($opt_concat and $sp ne "")) ? " " : "";
-        
         my $correct_label =($is_first or ((not $opt_concat and $sp ne "") or $opt_concat and $sp eq " ")) ? "1" : "0";
         
         my $possible_labels = "0 1";
@@ -76,17 +115,12 @@ while (<STDIN>) {
         }
         elsif ($preprocess and
                ($prev_preprocessed_char =~ m{$nonchar_regex} or
-                $preprocessed_char =~ m{$nonchar_regex})) {
-            if ($prev_preprocessed_char eq $preprocessed_char) {
+                $preprocessed_char =~ m{$nonchar_regex}) and
+               $prev_preprocessed_char eq $preprocessed_char) {
                 $possible_labels = "0";
-            }
-            elsif ($prev_preprocessed_char =~ m{$nonchar_regex} and
-                   $preprocessed_char =~ m{$nonchar_regex}) {
-                $possible_labels = "1";
-            }
-            else {
-                $possible_labels = "0 1";
-            }
+        }
+        else {
+            $possible_labels = "0 1";
         }
         
         my %possible_label_dict = ();
@@ -100,9 +134,9 @@ while (<STDIN>) {
             }
         }
 
-        print join("\t", $print_sp.$ch, $possible_labels, $correct_label)."\n";
+        print join("\t", $print_sp.$chunk, $possible_labels, $correct_label)."\n";
+        
         $is_first = 0;
-        $prev = $ch;
         $prev_preprocessed_char = $preprocessed_char;
     }
     print "\n";
